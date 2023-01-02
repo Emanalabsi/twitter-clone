8da6b1a8a25ccb1164b2ad422383b3a60b1542e2
EmanAlabsi <emanabsi30@gmail.com>
2023-01-02 16:46:50 +0200

    refactor create tweet element

--- main.js
+++ main.js
@@ -14,134 +14,87 @@ tweetButton.addEventListener("click", function () {
   }
 });
 
-function createTweet(val, author) {
+function createTweetElement(val, author) {
   //tweet time line container
-
   let tweetTimeline = document.createElement("div");
-
   tweetTimeline.className = "tweet-timeline";
-
-  tweets.prepend(tweetTimeline);
+  // tweets.prepend(tweetTimeline);
 
   //img of the profile
   let profileImg = document.createElement("img");
-
   profileImg.src = "/imgs/SelfPortrait3.jpeg";
-
   profileImg.alt = "profile-img";
-
   profileImg.className = "profile-img";
-
   tweetTimeline.append(profileImg);
-  //tweeter details
-  //then all will be appended to this div
 
   let tweeterDetails = document.createElement("div");
-
   tweeterDetails.className = "tweeter-details";
 
   let tweeterInfo = document.createElement("div");
-
-  //tweeterinfo
   tweeterInfo.className = "tweeter-info";
 
   let tweeterUsername = document.createElement("a");
-
   tweeterUsername.className = "tweeter-username";
 
   let tweetAuthor = author;
-
   tweeterUsername.append(tweetAuthor);
 
   let tweeterNickname = document.createElement("span");
-
   tweeterNickname.className = "tweeter-nickname";
 
   let nicknameText = document.createTextNode("@emanalabsi . 1h");
-
   tweeterNickname.append(nicknameText);
-
   tweeterUsername.append(tweeterNickname);
-
   tweeterInfo.append(tweeterUsername);
-
   let dotsIcon = document.createElement("i");
-
   dotsIcon.className = "material-icons-outlined";
-
   let dotsIconText = document.createTextNode("more_horiz");
-
   dotsIcon.append(dotsIconText);
-
   tweeterInfo.append(dotsIcon);
-
   tweeterDetails.append(tweeterInfo);
-
   let tweetText = document.createElement("div");
-
   tweetText.className = "tweet-text";
-
   let tweetContent = document.createElement("p");
-
   tweetContent.className = "tweet-content";
-
   tweetContent.append(val);
-
   tweetText.append(tweetContent);
-
   tweeterDetails.append(tweetText);
 
   //tweet icons
   let tweetIcons = document.createElement("div");
   tweetIcons.className = "tweet-icons";
-
-  let bubbleAnchor = document.createElement("a");
-
+  let bubbleAnchor = document.createElement("button");
   let bubbleIcon = document.createElement("i");
-
   bubbleIcon.className = "material-icons-outlined";
 
+  //1st icon
   let bubbleIconText = document.createTextNode("chat_bubble_outline");
-
   bubbleIcon.append(bubbleIconText);
-
   bubbleAnchor.append(bubbleIcon);
-
   tweetIcons.append(bubbleAnchor);
+
   //2nd icon
-  let retweetAnchor = document.createElement("a");
-
+  let retweetAnchor = document.createElement("button");
   retweetAnchor.className = "retweet-btn";
-
   let retweetIcon = document.createElement("i");
-
   retweetIcon.className = "material-icons-outlined";
-
   let retweetIconText = document.createTextNode("restart_alt");
-
   retweetIcon.append(retweetIconText);
-
   retweetAnchor.append(retweetIcon);
-
   tweetIcons.append(retweetAnchor);
+
   //3rd icon
-  let favAnchor = document.createElement("a");
-
+  let favAnchor = document.createElement("button");
   favAnchor.className = "like-btn";
-
   let favIcon = document.createElement("i");
-
   favIcon.className = "material-icons-outlined";
-
   let favIconText = document.createTextNode("favorite_border");
-
   favIcon.append(favIconText);
-
   favAnchor.append(favIcon);
-
   tweetIcons.append(favAnchor);
+
   //4th icon
-  let uploadAnchor = document.createElement("a");
+  let uploadAnchor = document.createElement("button");
 
   let uploadIcon = document.createElement("i");
 
@@ -150,21 +103,26 @@ function createTweet(val, author) {
   let uploadIconText = document.createTextNode("upload");
 
   uploadIcon.append(uploadIconText);
-
   uploadAnchor.append(uploadIcon);
-
   tweetIcons.append(uploadAnchor);
-
   tweeterDetails.append(tweetIcons);
-
   tweetTimeline.append(tweeterDetails);
 
-  let tweetObj = { author: val };
-  tweetsContentArray.push(tweetObj);
-  localStorage.setItem(author, val);
+  return tweetTimeline;
+}
+
+function createTweet(val, author) {
+  const tweetElement = createTweetElement(val, author);
+
+  console.log(tweetElement);
+
+  // let tweetObj = { author: val };
+  // tweetsContentArray.push(tweetObj);
+  // localStorage.setItem(author, val);
   // console.log(tweetObj);
   // console.log(tweetsContentArray);
 }
+
 let retweetButtons = document.querySelectorAll(".retweet-btn");
 for (let i = 0; i < retweetButtons.length; i++) {
   retweetButtons[i].addEventListener("click", function () {
